4d9d2fcf4e077c4399d61ff2f2df4010fd8ef6fa
NathaliaSarto <nathaliasarto3@gmail.com>
2017-03-05 12:04:02 -0300

    Update Principal.java
    main

--- Principal.java
+++ Principal.java
@@ -1,7 +1,13 @@
 public class  Principal{
      puclic static void main (String args []){
-
-
-}
+            Lista dup = New Lista();
+            dup.inserir(11);
+            dup.inserir(22);
+            dup.inserir(33);
+            dup.exibir();
+            System.out.println(" ");
+            dup.remover();
+            System.out.println(" ");
+    }
 
 }
