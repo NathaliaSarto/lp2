e0e4b78bd6ce2de5f6c04790b9fab89a12d373c4
NathaliaSarto <nathaliasarto3@gmail.com>
2017-03-06 22:40:54 -0300

    Update Circunferencia.java
    Circunferencia

---
+++ ep2/Circunferencia.java
@@ -0,0 +1,20 @@
+public class Circunferencia extends FiguraGeometrica{
+   private double raio; 	
+	
+   public void setRaio(double raio){
+      this.raio=raio;	   
+   }
+   public double getRaio(){
+	  return this.raio;
+   }
+   
+   
+   public double calcArea(){
+	   3,14 * (Math.pow(raio,2);
+   }
+   
+   public doule calcPeri(){
+	   2 * 3,14 * raio;
+   }
+
+}
