22533814ed1e5ae23de4d1282aa1c9711c472745
NathaliaSarto <nathaliasarto3@gmail.com>
2017-03-06 22:51:58 -0300

    Update Retangulo.java
    Retangulo

---
+++ ep2/Retangulo.java
@@ -0,0 +1,28 @@
+public class Retangulo extends FiguraGeometrica{
+   private double lado1, altura1; 	
+	
+   public void setLado1(double lado1){
+      this.lado1=lado1;	   
+   }
+   public void setAltura1(double altura1){
+      this.altura1=altura1;	   
+   }
+   
+   
+   public double getLado1(){
+	  return this.lado1;
+   }
+   public double getAltura1(){
+	  return this.altura1;
+   }
+   
+   
+   public double calcArea(){
+	   get.Lado1() * get.Altura1();
+   }
+   
+   public doule calcPeri(){
+	   (get.Lado1() * 2) + (get.Altura1() * 2);
+   }
+
+}	
