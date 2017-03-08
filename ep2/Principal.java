f91ed1664892ed49c57495255981387bf946d6a6
NathaliaSarto <nathaliasarto3@gmail.com>
2017-03-07 21:13:55 -0300

    Update Principal.java

---
+++ ep2/Principal.java
@@ -0,0 +1,30 @@
+public class Principal{
+	public static void main(String [] args){
+    
+	double triangulo = new Triangulo(); 
+  triangulo.setBase(10);
+	triangulo.setAlt (5);
+	triangulo.setLd1 (10);
+	triangulo.setLd2 (10);
+
+	double trapezio = new Trapezio();
+	trapezio.setBama(8);
+	trapezio.setBame(6);
+	trapezio.setH(3);
+	trapezio.setLad1(4);
+	trapezio.setLad2(4);
+	
+	double retangulo = new Retangulo();
+	retangulo.setLado1(7);
+	retangulo.setAltura1(5);
+	
+	double quadrado = new Quadrado();
+	quadrado.setLado(3);
+	
+	double circunferencia = new Circunferencia();
+	raio.setRaio(4);
+
+	}
+	
+	
+}
