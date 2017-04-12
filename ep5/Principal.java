3e65b95c398f44b9a28940a47f17304b05d13bcc
NathaliaSarto <nathaliasarto3@gmail.com>
2017-04-11 21:44:17 -0300

    Update Principal.java

--- ep5/Principal.java
+++ ep5/Principal.java
@@ -1,1 +1,11 @@
 class Principal{
+  public static void main(String []ars){
+    Livros l1 = new Livros.getInstancia();
+    Livros l2 = new Livros.getInstancia();
+    l1.totalLivrosClarisee++;
+    Livros l3 = new Livros.getInstancia();
+    l3.totalLivrosClarisse+10;
+    Livros l4 = new Livros.getInstancia();
+    System.out.println(l1,l2,l3,l4);
+  }
+}
